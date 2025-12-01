cbc66c616927115d9ecfc1161900b92965f93b55
JordanFotso <achillefotso43@gmail.com>
2025-12-01 04:07:44 +0100

    Log MONGO_URI and NODE_ENV in migration config
    Added console logs for MONGO_URI and NODE_ENV.

--- backend/migrate-mongo-config.js
+++ backend/migrate-mongo-config.js
@@ -1,8 +1,11 @@
 import 'dotenv/config'; // charge automatiquement les variables d'environnement
 
 const isAtlas = process.env.MONGO_URI?.startsWith("mongodb+srv://");
+    console.log("MONGO_URI =", process.env.MONGO_URI);
+    console.log("NODE_ENV =", process.env.NODE_ENV);
 
 const config = {
+  
     mongodb: {
         url: process.env.MONGO_URI
             ? isAtlas
